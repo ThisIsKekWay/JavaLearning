8d969b98ca16abbe547f0212db0487dc9353fd4d
Koha <BelkoZdorovo@yandex.ru>
2023-02-13 22:39:42 +0300

    HW7Ex2(KOT+HUMAN)

---
+++ src/main/java/Homeworks/HW7/Ex2/Space.java
@@ -0,0 +1,14 @@
+package Homeworks.HW7.Ex2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Space {
+    int position;
+
+    public Space(int position) {
+        this.position = position;
+    }
+}
