9384656dbf56b90a0169f0348ca7c6f62ce68c89
Koha <BelkoZdorovo@yandex.ru>
2023-02-18 13:34:13 +0300

    HW8

--- src/main/java/Homeworks/HW7/Ex2/Animal.java
+++ src/main/java/Homeworks/HW7/Ex2/Animal.java
@@ -1,7 +1,7 @@
 package Homeworks.HW7.Ex2;
 
 public  interface Animal {
-    public void call(Owner owner, Pet pet);
+    void call(Owner owner, Pet pet);
 
-    public void pet(Owner owner, Pet pet);
+    void pet(Owner owner, Pet pet);
 }
